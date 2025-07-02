db021402562cdccc50510339e2a71dab43da692c
Rakib Hossen <rakib.dev70@gmail.com>
2025-07-02 16:54:40 +0600

    check size mater package

--- src/screen/Auth/LoginScreen.tsx
+++ src/screen/Auth/LoginScreen.tsx
@@ -1,51 +1,13 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import Theme from '../../Theme/Theme';
-import ClearButtonIcon from '../../assets/icons/ClearButtonIcon';
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   return (
     <Theme>
-      {/* =============header design================== */}
-      <View style={styles.mainWapper}>
-        <View style={styles.secondartwapper}>
-          <Text style={styles.headertxt}>Welcome to One to One</Text>
-          <Text style={styles.headertxtsecondary}>
-            Enter your email below to login to your account
-          </Text>
-          {/* ================TEXT INPUTALL================ */}
-          <View style={styles.inputWrapper}>
-            <TextInput
-              placeholder="Email"
-              placeholderTextColor="#aaa"
-              style={styles.input}
-              value={email}
-              onChangeText={setEmail}
-            />
-            <TouchableOpacity onPress={() => setEmail('')}>
-              <ClearButtonIcon />
-            </TouchableOpacity>
-          </View>
-          {/* ===============onno==================== */}
-          <View style={styles.inputWrapper}>
-            <TextInput
-              placeholder="Password"
-              placeholderTextColor="#aaa"
-              secureTextEntry={true}
-              style={styles.input}
-              value={password}
-              onChangeText={setPassword}
-            />
-          </View>
-          {/* ===============LogInbtn===================== */}
-          <TouchableOpacity style={styles.loginBtn}>
-            <Text style={styles.loginBtntxt}>Login</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.forgetPasswapper}>
-            <Text style={styles.forgettxt}>Forgot Password? </Text>
-          </TouchableOpacity>
+      <View style={styles.mainWrapper}>
+        <View style={styles.secondaryWrapper}>
+          <Text className="text-red-900">Welcome to One to One</Text>
         </View>
       </View>
     </Theme>
@@ -55,74 +17,18 @@ const LoginScreen = () => {
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-  mainWapper: {
+  mainWrapper: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  secondartwapper: {
-    height: 439,
-    width: 486,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  secondaryWrapper: {
+    width: 400, // = scale(100)
+    height: 400, // = verticalScale(200)
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
     borderRadius: 10,
-  },
-  headertxt: {
-    fontSize: 32,
-    fontWeight: '700',
-    color: 'rgba(255, 255, 255, 1)',
-    marginLeft: 60,
-    marginRight: 40,
-    marginTop: 40,
-  },
-  headertxtsecondary: {
-    color: 'rgba(255, 255, 255, 1)',
-    fontSize: 16,
-    fontWeight: '400',
-    marginRight: 40,
-    marginLeft: 60,
-  },
-  inputWrapper: {
-    height: 44,
-    width: 406,
-    backgroundColor: 'rgba(0, 0, 0, 0.9)',
-    borderRadius: 12,
-    borderWidth: 1,
-    marginTop: 45,
-    marginLeft: 40,
-    flexDirection: 'row',
-    alignItems: 'center',
     justifyContent: 'center',
-    paddingLeft: 15,
-    paddingRight: 12,
-  },
-  input: {
-    flex: 1,
-    height: 48,
-    //backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    color: '#fff',
-  },
-  loginBtn: {
-    height: 44,
-    width: 406,
-    backgroundColor: 'rgba(255, 255, 255, 1)',
-    borderRadius: 12,
     alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 45,
-    marginLeft: 40,
-  },
-  loginBtntxt: {
-    fontSize: 17,
-    fontWeight: '600',
-    letterSpacing: 1,
-  },
-  forgetPasswapper: {
-    //borderWidth:3,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 17,
-  },
-  forgettxt: {
-    color: 'rgba(232, 232, 232, 1)',
+    paddingHorizontal: 20,
   },
 });
